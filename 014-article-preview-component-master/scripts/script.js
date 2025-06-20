59fd5e4545fed4bfcd5ac79dc12d371e71c549b6
Icaro Leonel <103190983+icaroryann@users.noreply.github.com>
2025-06-19 23:37:16 -0300

    Implemented viewport catcher

--- 014-article-preview-component-master/scripts/script.js
+++ 014-article-preview-component-master/scripts/script.js
@@ -1,3 +1,9 @@
+// Altura da viewport multiplicada por 1% para obter o valor em vh
+let vh = window.innerHeight * 0.01
+
+// Salva o valor --vh no :root do css
+document.documentElement.style.setProperty('--vh', `${vh}px`)
+
 const button = document.querySelector('#toggle-social-medias')
 button.addEventListener('click', function() {
     const footer = document.querySelector('#share')
