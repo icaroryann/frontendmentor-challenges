daf3b32e01b5a3d292cec2cee28e02e59d4e0705
Icaro Leonel <103190983+icaroryann@users.noreply.github.com>
2025-06-26 18:04:28 -0300

    Update gitignore and fix msg invalid email

--- 016-intro-component-with-signup-form-master/scripts/script.js
+++ 016-intro-component-with-signup-form-master/scripts/script.js
@@ -19,9 +19,21 @@ submit.addEventListener('submit', function(event) {
             placeHolder[i].removeAttribute('placeholder')
 
             submit[i].style.animation = '.2s linear 0s alternate error'
-        }else if (!emailRegex.test(submit[i].value)) {
+        }else if (submit[i].type == 'email' && !emailRegex.test(submit[i].value)) {
             event.preventDefault()
-            submit[i].style.borderColor = 'orange'
+            submit[i].style.borderColor = 'var(--Red)'
+
+            const msgError = submit.getElementsByTagName('p')
+            msgError[i].innerHTML = 'Looks like this is not an email'
+            msgError[i].style.display = 'block'
+
+            const iconError = submit.getElementsByTagName('img')
+            iconError[i].style.display = 'block'
+
+            const placeHolder = submit.getElementsByTagName('input')
+            placeHolder[i].removeAttribute('placeholder')
+
+            submit[i].style.animation = '.2s linear 0s alternate error'
         }
     }
 
