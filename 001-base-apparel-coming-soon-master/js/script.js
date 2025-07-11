34f80f52853307a4726958c0428ec1be3bcee68a
Icaro Leonel <103190983+icaroryann@users.noreply.github.com>
2025-07-11 00:06:05 -0300

    form finished

---
+++ 001-base-apparel-coming-soon-master/js/script.js
@@ -0,0 +1,17 @@
+document.querySelector('#form').addEventListener('submit', function(event) {
+    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const inputEmail = this.querySelector('input');
+
+    if (!regexEmail.test(inputEmail.value)) {
+        event.preventDefault();
+        showError(inputEmail);
+    }
+})
+
+function showError(input) {
+    const errorMsg = document.querySelector('#error-mensage');
+    const errorImg = document.querySelector('#icon-error');
+    errorMsg.style.display = 'block';
+    input.focus();
+    errorImg.style.display = 'block'
+}
