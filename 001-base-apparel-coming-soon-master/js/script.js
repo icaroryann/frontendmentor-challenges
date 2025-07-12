5396e5c282c81efcfca4bb8ec0a8434510816bc7
Icaro Leonel <103190983+icaroryann@users.noreply.github.com>
2025-07-11 21:29:36 -0300

    Challenge 001 finished

--- 001-base-apparel-coming-soon-master/js/script.js
+++ 001-base-apparel-coming-soon-master/js/script.js
@@ -13,5 +13,16 @@ function showError(input) {
     const errorImg = document.querySelector('#icon-error');
     errorMsg.style.display = 'block';
     input.focus();
-    errorImg.style.display = 'block'
+    errorImg.style.display = 'block';
 }
+
+function reportWindowSize() {
+    const main = document.querySelector('main');
+    const mainStyle = window.getComputedStyle(main);
+    const marginLeftMain = mainStyle.marginLeft;
+    document.documentElement.style.setProperty('--main-margin-left', marginLeftMain)
+}
+
+reportWindowSize()
+
+window.onresize = reportWindowSize;
