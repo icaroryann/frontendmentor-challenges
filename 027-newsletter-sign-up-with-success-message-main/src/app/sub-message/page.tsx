78410e6b05e295f7d1a47a2a5cbdeb271c8a3d8a
Icaro Leonel <103190983+icaroryann@users.noreply.github.com>
2025-09-02 07:54:03 -0300

    Update page.tsx

--- 027-newsletter-sign-up-with-success-message-main/src/app/sub-message/page.tsx
+++ 027-newsletter-sign-up-with-success-message-main/src/app/sub-message/page.tsx
@@ -4,38 +4,40 @@ import React, { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 
-function Sub() {
+function SubContent() {
   const searchParams = useSearchParams()
   const email = searchParams.get('email')
 
   return (
     email 
     ?
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className='min-h-screen grid place-items-center p-6 md:bg-white md:min-h-auto md:w-md md:p-10 md:rounded-4xl'>
-          <div>
-            <img src="/assets/images/icon-success.svg" alt="Success icon" />
-            <h1
-              className='mt-4 text-4xl font-bold'
-              >Thanks for subscribing!</h1>
-            <p
-              className='mt-4'
-            >A confirmation email has been sent to <strong>{email}</strong>. Prease open it and click the button inside to confirm your subscription</p>
-          </div>
-          <Link 
-            className="mt-4 text-center bg-gray-900 text-white w-full py-4 rounded-md self-end hover:bg-gradient-to-r from-pink-500 to-orange-500 hover:shadow-xl hover:shadow-pink-500/40 transition-all duration-300 ease-in-out"
-            href='/'
-          > Dismiss message
-          </Link>
-        </div>
-      </Suspense>
-    :
-      <Suspense fallback={<div>Loading...</div>}>
+      <div className='min-h-screen grid place-items-center p-6 md:bg-white md:min-h-auto md:w-md md:p-10 md:rounded-4xl'>
         <div>
-          <h1>You should not be here without a email :)</h1>
+          <img src="/assets/images/icon-success.svg" alt="Success icon" />
+          <h1
+            className='mt-4 text-4xl font-bold'
+            >Thanks for subscribing!</h1>
+          <p
+            className='mt-4'
+          >A confirmation email has been sent to <strong>{email}</strong>. Prease open it and click the button inside to confirm your subscription</p>
         </div>
-      </Suspense>
+        <Link 
+          className="mt-4 text-center bg-gray-900 text-white w-full py-4 rounded-md self-end hover:bg-gradient-to-r from-pink-500 to-orange-500 hover:shadow-xl hover:shadow-pink-500/40 transition-all duration-300 ease-in-out"
+          href='/'
+        > Dismiss message
+        </Link>
+      </div>
+    :
+      <div>
+        <h1>You should not be here without a email :)</h1>
+      </div>
   )
 }
 
-export default Sub
+export default function Sub() {
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+      <SubContent />
+    </Suspense>
+  )
+}
